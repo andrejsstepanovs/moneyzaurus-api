016d477516f3730af8be3df61240d666f4386620
Andrejs Stepanovs <andrejsstepanovs@gmail.com>
2014-05-21 21:15:17 +0200

    db structure update

--- data/app.sql
+++ data/app.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.0-dev
+-- version 4.0.5
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: May 16, 2014 at 07:51 PM
--- Server version: 5.5.35-0ubuntu0.13.10.2-log
--- PHP Version: 5.5.3-1ubuntu2.1
+-- Host: 127.7.40.2:3306
+-- Generation Time: May 21, 2014 at 07:13 PM
+-- Server version: 5.5.36
+-- PHP Version: 5.3.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -25,29 +25,20 @@ USE `app`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `access_token`
---
-
-CREATE TABLE IF NOT EXISTS `access_token` (
-`access_token_id` int(10) unsigned NOT NULL,
-  `id_user` int(10) unsigned NOT NULL,
-  `token` varchar(40) NOT NULL,
-  `created` datetime NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `connection`
 --
 
 CREATE TABLE IF NOT EXISTS `connection` (
-`connection_id` int(10) unsigned NOT NULL,
+  `connection_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `id_user` int(10) unsigned NOT NULL,
   `id_user_parent` int(10) unsigned NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `state` enum('rejected','accepted') NOT NULL DEFAULT 'rejected'
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=168 ;
+  `state` enum('rejected','accepted') NOT NULL DEFAULT 'rejected',
+  PRIMARY KEY (`connection_id`),
+  UNIQUE KEY `id_user-id_user_parent` (`id_user`,`id_user_parent`),
+  KEY `id_user_parent` (`id_user_parent`),
+  KEY `status` (`state`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -59,7 +50,8 @@ CREATE TABLE IF NOT EXISTS `currency` (
   `currency_id` varchar(3) NOT NULL,
   `name` varchar(20) NOT NULL,
   `html` varchar(10) NOT NULL COMMENT 'html symbol',
-  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY `currency` (`currency_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -69,11 +61,15 @@ CREATE TABLE IF NOT EXISTS `currency` (
 --
 
 CREATE TABLE IF NOT EXISTS `group` (
-`group_id` int(10) unsigned NOT NULL,
+  `group_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `id_user` int(10) unsigned NOT NULL,
   `name` varchar(255) NOT NULL,
-  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=448 ;
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`group_id`),
+  KEY `id_user` (`id_user`),
+  KEY `name` (`name`),
+  KEY `date_created` (`date_created`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -82,11 +78,14 @@ CREATE TABLE IF NOT EXISTS `group` (
 --
 
 CREATE TABLE IF NOT EXISTS `item` (
-`item_id` int(10) unsigned NOT NULL,
+  `item_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `id_user` int(10) unsigned NOT NULL,
   `name` varchar(255) NOT NULL,
-  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4297 ;
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`item_id`),
+  UNIQUE KEY `name-id_user` (`name`,`id_user`),
+  KEY `id_user` (`id_user`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -95,15 +94,22 @@ CREATE TABLE IF NOT EXISTS `item` (
 --
 
 CREATE TABLE IF NOT EXISTS `transaction` (
-`transaction_id` int(10) unsigned NOT NULL,
+  `transaction_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `id_user` int(10) unsigned NOT NULL,
   `id_group` int(10) unsigned NOT NULL,
   `id_item` int(10) unsigned NOT NULL,
-  `price` smallint(9) NOT NULL,
+  `price` int(10) NOT NULL,
   `id_currency` varchar(3) NOT NULL,
   `date` date NOT NULL,
-  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24619 ;
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`transaction_id`),
+  KEY `id_user` (`id_user`),
+  KEY `id_group` (`id_group`),
+  KEY `id_item` (`id_item`),
+  KEY `id_currency` (`id_currency`),
+  KEY `date_transaction` (`date`),
+  KEY `amount` (`price`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -112,17 +118,21 @@ CREATE TABLE IF NOT EXISTS `transaction` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-`user_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `role` enum('user','admin') NOT NULL DEFAULT 'user',
   `username` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `display_name` varchar(50) DEFAULT NULL,
   `password` varchar(128) NOT NULL,
-  `locale` varchar(5) NOT NULL DEFAULT 'de_DE',
+  `locale` varchar(5) NOT NULL DEFAULT 'en_US',
   `timezone` varchar(40) NOT NULL DEFAULT 'Europe/Berlin',
   `language` varchar(5) NOT NULL DEFAULT 'en_US',
-  `state` smallint(5) unsigned DEFAULT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=161 ;
+  `state` smallint(5) unsigned DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  KEY `role` (`role`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -133,138 +143,48 @@ CREATE TABLE IF NOT EXISTS `user` (
 CREATE TABLE IF NOT EXISTS `user_provider` (
   `user_id` int(10) unsigned NOT NULL,
   `provider_id` varchar(50) NOT NULL,
-  `provider` varchar(255) NOT NULL
+  `provider` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`,`provider_id`),
+  UNIQUE KEY `provider_id` (`provider_id`,`provider`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `access_token`
---
-ALTER TABLE `access_token`
- ADD PRIMARY KEY (`access_token_id`), ADD UNIQUE KEY `token` (`token`), ADD KEY `id_user` (`id_user`);
-
---
--- Indexes for table `connection`
---
-ALTER TABLE `connection`
- ADD PRIMARY KEY (`connection_id`), ADD UNIQUE KEY `id_user-id_user_parent` (`id_user`,`id_user_parent`), ADD KEY `id_user_parent` (`id_user_parent`), ADD KEY `status` (`state`);
-
---
--- Indexes for table `currency`
---
-ALTER TABLE `currency`
- ADD PRIMARY KEY (`currency_id`), ADD UNIQUE KEY `currency` (`currency_id`);
-
---
--- Indexes for table `group`
---
-ALTER TABLE `group`
- ADD PRIMARY KEY (`group_id`), ADD KEY `id_user` (`id_user`), ADD KEY `name` (`name`), ADD KEY `date_created` (`date_created`);
-
---
--- Indexes for table `item`
---
-ALTER TABLE `item`
- ADD PRIMARY KEY (`item_id`), ADD UNIQUE KEY `name-id_user` (`name`,`id_user`), ADD KEY `id_user` (`id_user`);
-
---
--- Indexes for table `transaction`
---
-ALTER TABLE `transaction`
- ADD PRIMARY KEY (`transaction_id`), ADD KEY `id_user` (`id_user`), ADD KEY `id_group` (`id_group`), ADD KEY `id_item` (`id_item`), ADD KEY `price` (`price`), ADD KEY `id_currency` (`id_currency`), ADD KEY `date_transaction` (`date`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
- ADD PRIMARY KEY (`user_id`), ADD UNIQUE KEY `username` (`username`), ADD UNIQUE KEY `email` (`email`), ADD KEY `role` (`role`);
-
---
--- Indexes for table `user_provider`
---
-ALTER TABLE `user_provider`
- ADD PRIMARY KEY (`user_id`,`provider_id`), ADD UNIQUE KEY `provider_id` (`provider_id`,`provider`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `access_token`
---
-ALTER TABLE `access_token`
-MODIFY `access_token_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT for table `connection`
---
-ALTER TABLE `connection`
-MODIFY `connection_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=168;
---
--- AUTO_INCREMENT for table `group`
---
-ALTER TABLE `group`
-MODIFY `group_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=448;
---
--- AUTO_INCREMENT for table `item`
---
-ALTER TABLE `item`
-MODIFY `item_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4297;
---
--- AUTO_INCREMENT for table `transaction`
---
-ALTER TABLE `transaction`
-MODIFY `transaction_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24619;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-MODIFY `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=161;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `access_token`
---
-ALTER TABLE `access_token`
-ADD CONSTRAINT `access_token_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Constraints for table `connection`
 --
 ALTER TABLE `connection`
-ADD CONSTRAINT `connection_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `connection_ibfk_2` FOREIGN KEY (`id_user_parent`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `connection_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `connection_ibfk_2` FOREIGN KEY (`id_user_parent`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `group`
 --
 ALTER TABLE `group`
-ADD CONSTRAINT `group_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `group_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `item`
 --
 ALTER TABLE `item`
-ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `transaction`
 --
 ALTER TABLE `transaction`
-ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`id_group`) REFERENCES `group` (`group_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`id_item`) REFERENCES `item` (`item_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `transaction_ibfk_4` FOREIGN KEY (`id_currency`) REFERENCES `currency` (`currency_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`id_group`) REFERENCES `group` (`group_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`id_item`) REFERENCES `item` (`item_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaction_ibfk_4` FOREIGN KEY (`id_currency`) REFERENCES `currency` (`currency_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user_provider`
 --
 ALTER TABLE `user_provider`
-ADD CONSTRAINT `user_provider_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+  ADD CONSTRAINT `user_provider_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
