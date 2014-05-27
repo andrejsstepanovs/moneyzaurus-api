387e2859b56fdef8ffc6f84575b803f2980d8b5c
Andrejs Stepanovs <andrejsstepanovs@gmail.com>
2014-05-27 21:27:42 +0200

    doctrine logger

---
+++ src/Service/Doctrine/SQLLogger.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Api\Service\Doctrine;
+
+use \Doctrine\DBAL\Logging\SQLLogger as LoggerInterface;
+
+/**
+ * Class SQLLogger
+ *
+ * @package Api\Service
+ */
+class SQLLogger implements LoggerInterface
+{
+    /**
+     * Logs a SQL statement somewhere.
+     *
+     * @param string     $sql    The SQL to be executed.
+     * @param array|null $params The SQL parameters.
+     * @param array|null $types  The SQL parameter types.
+     *
+     * @return void
+     */
+    public function startQuery($sql, array $params = null, array $types = null)
+    {
+
+    }
+
+    /**
+     * Marks the last started query as stopped. This can be used for timing of queries.
+     *
+     * @return void
+     */
+    public function stopQuery()
+    {
+
+    }
+}
