f27128a8de2054f090e9fbe33a8987068e3dafa2
Andrejs Stepanovs <andrejsstepanovs@gmail.com>
2014-05-31 20:31:16 +0200

    install nginx on travis

--- tests/acceptance/bootstrap.php
+++ tests/acceptance/bootstrap.php
@@ -2,10 +2,18 @@
 
 use AcceptanceTests\Bootstrap;
 
-//require_once '/var/www/lib/auto_prepend_file.php';
+require_once '/var/www/lib/auto_prepend_file.php';
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
+
+define('WEB_SERVER_HOST', 'localhost');
+define('WEB_SERVER_PORT', '8000');
+define('WEB_SERVER_DOCROOT', __DIR__ . '/../../');
+define('HHVM_PORT', '8100');
+define('PHP_INI_FILE', __DIR__ . '/php.ini');
+
 
 $params = array(
     'config_original' => __DIR__ . '/../../config/config.php',
