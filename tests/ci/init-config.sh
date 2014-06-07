d6e56461413208e55cadf3e5ea3a182a504122da
Andrejs Stepanovs <andrejsstepanovs@gmail.com>
2014-06-07 17:22:48 +0200

    init config variables

---
+++ tests/ci/init-config.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# https://github.com/facebook/hhvm/wiki/fastcgi
+
+export WEB_SERVER_HOST=localhost
+export WEB_SERVER_PORT=8000
+export WEB_SERVER_DOCROOT=${PWD}
--- tests/ci/start-php.sh
+++ tests/ci/start-php.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # https://github.com/facebook/hhvm/wiki/fastcgi
 
+echo $WEB_SERVER_DOCROOT
+
 php --version
 
 php -S localhost:8000 -t ./ &
