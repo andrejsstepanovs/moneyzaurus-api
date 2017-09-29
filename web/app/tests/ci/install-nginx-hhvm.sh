b63c38267597e11dc69d02a20d123fbc92c976ba
Andrejs Stepanovs <a.stepanovs@vaola.de>
2017-09-29 14:20:24 +0200

    remove ci

--- web/app/tests/ci/install-nginx-hhvm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# https://github.com/facebook/hhvm/wiki/fastcgi
-
-sudo apt-get install nginx
-
-# Configure apache virtual hosts
-sudo cp -f tests/ci/hhvm-nginx.conf /etc/nginx/nginx.conf
-sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/nginx/nginx.conf
--- web/app/tests/ci/start-php.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# https://github.com/facebook/hhvm/wiki/fastcgi
-
-php --version
-
-php -S localhost:8000 -t ./ &
